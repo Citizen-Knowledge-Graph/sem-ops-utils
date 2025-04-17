23d27e7d2328843a79543214a7fcfb795fcbf7ca
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-04-17 18:26:45 +0200

    Add timestamp getters

--- src/main.js
+++ src/main.js
@@ -167,3 +167,15 @@ export function shrink(fullUri) {
 export function quadToTriple(quad) {
     return { s: quad.subject.value, p: quad.predicate.value, o: quad.object.value }
 }
+
+export function getTimestamp(compact = false) {
+    const now = new Date()
+    const pad = n => n.toString().padStart(2, "0")
+    if(compact) return `${pad(now.getFullYear())}${pad(now.getMonth() + 1)}${pad(now.getDate())}${pad(now.getHours())}${pad(now.getMinutes())}${pad(now.getSeconds())}`
+    return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())}_${pad(now.getHours())}-${pad(now.getMinutes())}-${pad(now.getSeconds())}`
+}
+
+export function getTimestampAsLiteral() {
+    const now = new Date().toISOString()
+    return rdf.literal(now, rdf.namedNode("http://www.w3.org/2001/XMLSchema#dateTime"))
+}

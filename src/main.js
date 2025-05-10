9553aadcd3d1b22cf6095a5192bff4e8be3a1ec5
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-05-10 14:23:06 +0200

    Add 2 prefixes

--- src/main.js
+++ src/main.js
@@ -17,7 +17,9 @@ export const prefixes = {
     ff: "https://foerderfunke.org/default#",
     sh: "http://www.w3.org/ns/shacl#",
     xsd: "http://www.w3.org/2001/XMLSchema#",
-    rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    rdfs: "http://www.w3.org/2000/01/rdf-schema#",
+    schema: "http://schema.org/",
 }
 
 const prefixesArr = Object.entries(prefixes).map(

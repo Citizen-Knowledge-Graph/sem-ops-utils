e62534f36c22def9a4fc351fb41b95cdb601b047
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-11-17 23:44:25 +0100

    Add shn prefix

--- src/main.js
+++ src/main.js
@@ -28,7 +28,8 @@ export const prefixes = {
     xsd: "http://www.w3.org/2001/XMLSchema#",
     rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     rdfs: "http://www.w3.org/2000/01/rdf-schema#",
-    schema: "http://schema.org/",
+    shn: "https://schemas.link/shacl-next#>",
+    schema: "http://schema.org/"
 }
 
 const prefixesArr = Object.entries(prefixes).map(

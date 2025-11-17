7e75c482e8818f9bd250a85abb428460d1ce0b0e
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-11-17 23:46:30 +0100

    Fix typo

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ export const prefixes = {
     xsd: "http://www.w3.org/2001/XMLSchema#",
     rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     rdfs: "http://www.w3.org/2000/01/rdf-schema#",
-    shn: "https://schemas.link/shacl-next#>",
+    shn: "https://schemas.link/shacl-next#",
     schema: "http://schema.org/"
 }
 
